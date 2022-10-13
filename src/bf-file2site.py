59f26120ee92862f625089883724748c931a9636
Michael Schwarz <mschwarz@multitool.net>
2022-10-13 15:13:16 -0500

    Stub the main

---
+++ src/bf-file2site.py
@@ -0,0 +1,11 @@
+"""bf-file2site.py - A utility to upload a file to a BigFix
+site via the REST API"""
+
+def main():
+    pass
+
+# Convention for possible future module/import
+if __name__ == "__main__":
+    main()
+    exit(0)
+
