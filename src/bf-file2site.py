ffde9507b0689c8a59b263a17151b209c9060205
Michael Schwarz <mschwarz@multitool.net>
2022-10-13 15:20:02 -0500

    Workpoint

--- src/bf-file2site.py
+++ src/bf-file2site.py
@@ -1,7 +1,23 @@
 """bf-file2site.py - A utility to upload a file to a BigFix
 site via the REST API"""
+import argparse
+
+# The following is just for warning suppression
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# end of warning suppression
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--bfserver", type=str, help="BigFix REST Server name/IP address"
+    )
+    parser.add_argument(
+        "-p", "--bfport", type=int, help="BigFix Port number (default 52311)", default=52311
+    )
+    parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name")
+    parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password")
     pass
 
 # Convention for possible future module/import
